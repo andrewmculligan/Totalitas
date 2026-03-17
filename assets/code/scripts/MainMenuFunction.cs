ef7535a1873f959f7447da72dd00a70224f9bd8b
Tatabbyi <ytrachel0@gmail.com>
2026-03-17 13:58:08 +0000

    fix: Typos in MainMenu script

--- assets/code/scripts/MainMenuFunction.cs
+++ assets/code/scripts/MainMenuFunction.cs
@@ -1,28 +1,30 @@
 using UnityEngine;
 using System.Collections;
-using System.Collection.Generic;
-using unityEngine.UIElements;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 public class MainMenuFunction : MonoBehaviour
 {
-    private UIDocument  _document;
-
-    private Button  _button;
+    private UIDocument _document;
+    private Button _button;
 
     private void Awake()
     {
-         _document = GetComponent<UIDocument>();
+        _document = GetComponent<UIDocument>();
 
-         _button = _document.rootVisualElement.Q("Start") as button:
-         _button.RegisterCallBack<ClickEvent>(OnPlayGameClick)
+        // Use Q<Button> to query by name
+        _button = _document.rootVisualElement.Q<Button>("Start");
+        _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
     }
-        private void OnDisable()
+
+    private void OnDisable()
     {
-        _button.UnregisterCallBack<ClickEvent>(OnPlayGameClick);
+        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
     }
+
     private void OnPlayGameClick(ClickEvent evt)
     {
         Debug.Log("You pressed the start button");
     }
-
 }
+
